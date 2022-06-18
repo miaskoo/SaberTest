24348c8deef303f3a214c43dec3bfb6d04d7557c
Igor Losev <Call-Miasko97-ctaptor@yandex.ru>
2022-06-18 17:21:06 +0700

    add some faster solution for task 1

--- source/task1.cpp
+++ source/task1.cpp
@@ -1,36 +1,71 @@
 #include "task1.h"
 #include <stdio.h>
+#include <stddef.h>
 
-char* test::intToBit(int value) {
-	char* result;
-	char zero = '0';
-	char one = '1';
-	char minus = '-';
-	auto countCharacters = 1U;
-	bool isMinus = value < 0;
-	if (isMinus) {
-		countCharacters++;
-		value *= -1;
-	}
+using namespace test;
 
-	for (int bit = 2; bit <= value; bit *= 2) {
-		countCharacters++;
+listBit::listBit() {
+	head = nullptr;
+	tail = nullptr;
+}
+
+listBit::~listBit() {
+	while (head) {
+		auto next = head->next;
+		delete head;
+		head = head->next;
 	}
-	
-	result = new char[countCharacters + 1U];
-	result[countCharacters] = '\0';
-	for (int n = isMinus ? 1 : 0; n < countCharacters; n++) {
-		result[n] = value % 2 ? one : zero;
+}
+
+void listBit::pushBack(bool value) {
+	if (tail) {
+		tail->next = new bitNode();
+		tail = tail->next;
+		tail->data = value;
+		tail->next = nullptr;
+	}
+	else {
+		head = new bitNode();
+		tail = head;
+		tail->data = value;
+		tail->next = nullptr;
+	}
+}
+
+bitNode const* listBit::getHead() {
+	return head;
+}
+
+listBit* test::intToBit(unsigned int value) {
+	listBit* list = new listBit;
+	auto startValue = value;
+	do {
+		list->pushBack(value & 1);
 		value = value >> 1;
 	}
-	if (isMinus) {
-		result[0] = minus;
-	}
-	return result;
+	while (value);
+
+	return list;
 }
 
 void test::printBit(int value) {
-	auto bitValue = test::intToBit(value);
-	printf("%s\n", bitValue);
-	delete[] bitValue;
+	bool isMinus = value < 0;
+	if (isMinus) {
+		value *= -1;
+	}
+	auto list = test::intToBit(static_cast<unsigned int>(value));
+	auto bits = list->getHead();
+	const char* zero = "0";
+	const char* one = "1";
+	const char* minus = "-";
+	if (isMinus) {
+		printf("%s", minus);
+	}
+	while (bits) {
+		printf("%s", bits->data ? one : zero);
+		bits = bits->next;
+	}
+	printf("\n");
+	
+	delete list;
 }
--- source/task1.h
+++ source/task1.h
@@ -2,7 +2,24 @@
 #define task1_h
 
 namespace test {
-	char* intToBit(int value);
+
+	struct bitNode {
+		bool data;
+		bitNode* next;
+	};
+
+	class listBit {
+		public:
+			listBit();
+			~listBit();
+			void pushBack(bool value);
+			bitNode const* getHead();
+		private:
+			bitNode* head;
+			bitNode* tail;
+	};
+
+	listBit* intToBit(unsigned int value);
 	void printBit(int value);
 }
 
